2fb5b94691d65448006e70251b282847ae9d0e93
grdn1337 <dev@grdn.rocks>
2022-04-05 13:42:58 +0200

    added dependency and raised version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pypaperless",
-    version="0.0.1",
+    version="0.0.2",
     author="tbsch",
     author_email="info@tbsch.de",
     description="A small API wrapper for paperless-ngx dms.",
