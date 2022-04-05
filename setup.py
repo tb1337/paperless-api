467387295ef2c4a8e5d5cf712ed193234e5a6ecf
grdn1337 <dev@grdn.rocks>
2022-04-05 16:59:11 +0200

    edited setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pypaperless",
-    version="0.0.4",
+    version="0.0.5",
     author="tbsch",
     author_email="info@tbsch.de",
     description="A small API wrapper for paperless-ngx dms.",
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "pypaperless"},
+    # package_dir={"": "pypaperless"},
     packages=setuptools.find_packages(where="pypaperless"),
     python_requires=">=3.6",
 )
