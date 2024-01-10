5cd64cf0328607c144356ce7060be27308743bc0
tb1337 <rubbery.00.minster@icloud.com>
2024-01-10 16:24:40 +0000

    Add more tests

--- tests/test_paperless_0_0_0.py
+++ tests/test_paperless_0_0_0.py
@@ -2,6 +2,9 @@
 
 from pypaperless import Paperless
 from pypaperless.controllers import CorrespondentsController
+from pypaperless.controllers.base import ResultPage
+from pypaperless.models import Correspondent, CorrespondentPost
+from pypaperless.models.matching import MatchingAlgorithm
 
 
 class TestPaperlessV00:
@@ -19,13 +22,13 @@ class TestPaperlessV00:
         """Test features."""
         # basic class has no features
         assert api_00.features == 0
-        assert api_00.storage_paths is None
-        assert api_00.consumption_templates is None
-        assert api_00.custom_fields is None
-        assert api_00.share_links is None
-        assert api_00.workflows is None
-        assert api_00.workflow_actions is None
-        assert api_00.workflow_triggers is None
+        assert not api_00.storage_paths
+        assert not api_00.consumption_templates
+        assert not api_00.custom_fields
+        assert not api_00.share_links
+        assert not api_00.workflows
+        assert not api_00.workflow_actions
+        assert not api_00.workflow_triggers
 
     async def test_correspondents(self, api_00: Paperless):
         """Test correspondents."""
@@ -38,9 +41,67 @@ class TestPaperlessV00:
         assert getattr(api_00.correspondents, "create")
         assert getattr(api_00.correspondents, "update")
         assert getattr(api_00.correspondents, "delete")
-        # test list
+
+    async def test_correspondents_list(self, api_00: Paperless):
+        """Test correspondents list."""
         items = await api_00.correspondents.list()
         assert isinstance(items, list)
         assert len(items) > 0
         for item in items:
             assert isinstance(item, int)
+
+    async def test_correspondents_get(self, api_00: Paperless):
+        """Test correspondents get."""
+        results = await api_00.correspondents.get()
+        assert isinstance(results, ResultPage)
+        assert results.current_page == 1
+        assert not results.next_page  # there is 1 page in sample data
+        assert results.last_page == 1  # there is 1 page in sample data
+        assert isinstance(results.items, list)
+        for item in results.items:
+            assert isinstance(item, Correspondent)
+
+    async def test_correspondents_iterate(self, api_00: Paperless):
+        """Test correspondents iterate."""
+        async for item in api_00.correspondents.iterate():
+            assert isinstance(item, Correspondent)
+
+    async def test_correspondents_one(self, api_00: Paperless):
+        """Test correspondents one."""
+        item = await api_00.correspondents.one(1)
+        assert item
+        assert isinstance(item, Correspondent)
+
+    async def test_correspondents_create(self, api_00: Paperless):
+        """Test correspondents create."""
+        to_create = CorrespondentPost(name="Created Correspondent")
+        # test mixins, and their defaults
+        assert to_create.is_insensitive is True
+        assert to_create.match == ""
+        assert to_create.matching_algorithm == MatchingAlgorithm.NONE
+        # test default override
+        to_create = CorrespondentPost(
+            name="Created Correspondent",
+            matching_algorithm=MatchingAlgorithm.FUZZY,
+        )
+        assert to_create.matching_algorithm == MatchingAlgorithm.FUZZY
+        # actually call the create endpoint
+        created = await api_00.correspondents.create(to_create)
+        assert isinstance(created, Correspondent)
+        assert created.id == 6
+        assert created.matching_algorithm == MatchingAlgorithm.FUZZY
+
+    async def test_correspondents_udpate(self, api_00: Paperless):
+        """Test correspondents update."""
+        new_name = "Created Correspondent Update"
+        to_update = await api_00.correspondents.one(6)
+        to_update.name = new_name
+        updated = await api_00.correspondents.update(to_update)
+        assert isinstance(updated, Correspondent)
+        assert updated.name == new_name
+
+    async def test_correspondent_delete(self, api_00: Paperless):
+        """Test correspondent delete."""
+        to_delete = await api_00.correspondents.one(6)
+        deleted = await api_00.correspondents.delete(to_delete)
+        assert deleted
